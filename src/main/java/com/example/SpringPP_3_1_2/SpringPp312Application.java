cfb407ed5b08ae36136dde647bd2fc6ca8602748
VladislavPopkov <105574941+VladislavPopkov@users.noreply.github.com>
2022-08-10 20:37:14 +1100

    done

---
+++ src/main/java/com/example/SpringPP_3_1_2/SpringPp312Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringPP_3_1_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPp312Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPp312Application.class, args);
+	}
+
+}
